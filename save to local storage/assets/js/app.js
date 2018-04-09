dea3783e21fc61adb560032011bddd44961904ba
jasonduquain <jduquain@gmail.com>
2018-04-09 13:55:54 -0400

    starting save to localStorage app

---
+++ save to local storage/assets/js/app.js
@@ -0,0 +1,110 @@
+//// Variables
+const tweetList = document.querySelector('#tweet-list');
+
+
+//// Event Listeners
+
+
+function eventListeners() {
+    /// Form Submission
+    
+    
+    /// Remove tweet from the list
+    
+}
+
+
+///// Functions
+
+function newTweet(e) {
+    
+    
+    /// Read the textarea value
+    
+    
+    /// Create the remove btn
+    
+    
+    /// Create an LI element
+    
+    
+    /// Add the remove btn to each tweet
+    
+    
+    /// Add to the list
+    
+    
+    /// Add to local storage
+    
+    
+    // Print the alert
+    
+    
+    
+    
+}
+
+/// Remove the tweets from the DOM
+function removeTweet(e) {
+    
+
+    /// Remove from storage
+    
+}
+
+/// Adds the tweets into the local storage
+function addTweetLocalStorage(tweet) {
+    
+    
+    /// Add the tweets into the arr
+    
+    
+    /// Convert arr to stg
+    
+}
+
+function getTweetsFromStorage() {
+    
+    // Get the values, if null is returned we create an empty arr
+    
+}
+
+/// Prints local storage tweets on load
+function localStorageOnLoad() {
+    
+    
+    /// Loop thru storage and then print the values
+    //tweets.forEach(function(tweet) {
+        /// Create the remove btn
+        
+
+        /// Create an LI element
+        
+
+        /// Add the remove btn to each tweet
+        
+
+        /// Add to the list
+        
+    //});
+}
+
+function removeTweetLocalStorage(tweet) {
+    //get Tweets from storage
+    
+    
+    // Remove the 'X' from the tweets
+    
+    
+    /// Look thru the tweets and remove the tweet thats equal
+    
+    
+    // Save the data
+    
+}
+
+
+
+
+
+
