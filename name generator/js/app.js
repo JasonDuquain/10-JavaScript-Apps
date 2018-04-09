971b18a1278d600cead13537a8546fa65e76dbcb
jasonduquain <jduquain@gmail.com>
2018-04-09 11:03:36 -0400

    name generator app complete

--- name generator/js/app.js
+++ name generator/js/app.js
@@ -3,6 +3,7 @@ document.querySelector('#generate-names').addEventListener('submit', loadNames);
 
 // Execute the fn to query the API
 function loadNames(e) {
+    e.preventDefault();
     
     
     // Read the values from the form and create the variables
@@ -14,25 +15,48 @@ function loadNames(e) {
     let url = 'http://uinames.com/api/?';
     
     // Read the origin and append the URL
-    
+    if (origin !== '') {
+        url += `region=${origin}&`;
+    }
     
     // Read the genre and append the URL
-    
+    if (genre !== '') {
+        url += `gender=${genre}&`;
+    }
     
     // Read the amount and append the URL
-    
+    if (amount !== '') {
+        url += `amount=${amount}&`;
+    }
     
     // AJAX call
-    
+    const xhr = new XMLHttpRequest();
     
     // Open the connection
-    
+    xhr.open('GET', url, true);
     
     // Execute the fn
     xhr.onload = function() {
-        
+        if (this.status === 200) {
+            const names = JSON.parse(this.responseText);
+            
+            let html = '<h2>Generated Names</h2>';
+            html += '<ul class="list">';
+            names.forEach(function(name) {
+                html += `
+                    <li>${name.name} Duquain</li>  
+                `;
+                    
+            });
+            
+            html += '</ul>';
+            
+            document.querySelector('#result').innerHTML = html;
+            
+        }
     }
     
     // Send the request
+    xhr.send();
     
 }
