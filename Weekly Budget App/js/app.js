e4f8a6b1618144c5d451d7522118f23c95616ce1
jasonduquain <jduquain@gmail.com>
2018-04-22 14:33:55 -0400

    weekly budget app complete

--- Weekly Budget App/js/app.js
+++ Weekly Budget App/js/app.js
@@ -5,13 +5,13 @@ class Budget {
         this.budget = Number(budget);
         this.budgetLeft = this.budget;
     }
+    
+    subtractFromBudget(amount) {
+        return this.budgetLeft -= amount;
+    }
 }
 
 class HTML {
-    constructor() {
-        
-    }
-    
     insertBudget(amount) {
         budgetTotal.innerHTML = `${amount}`;
         budgetLeft.innerHTML = `${amount}`;
@@ -22,8 +22,38 @@ class HTML {
         messageWrapper.classList.add('text-center', 'alert', className);
         messageWrapper.append(document.createTextNode(message));
         document.querySelector('.primary').insertBefore(messageWrapper, addExpenseForm);
+        setTimeout(() => {
+            document.querySelector('.primary .alert').remove();
+            addExpenseForm.reset();
+        }, 1111);
+    }
+    
+    addExpenseToList(name, amount) {
+        const expensesList = document.querySelector('#expenses ul');
+        const li = document.createElement('li');
+        li.className = 'list-group-item d-flex justify-content-between align-items-center';
+        li.innerHTML = `
+            ${name}
+            <span class="badge badge-primary badge-pill">$${amount}</span>
+        `;
+        expensesList.append(li);
+    }
+    
+    trackBudget(amount) {
+        const budgetLeftDollars = budget.subtractFromBudget(amount);
+        budgetLeft.innerHTML = `${budgetLeftDollars}`;
         
-        setTimeout(() => document.querySelector('.primary .alert').remove(), 1111)
+        // check when 25% left or less
+        if ((budget.budget / 4) > budgetLeftDollars) {
+            budgetLeft.parentElement.parentElement.classList.remove('alert-success', 'alert-warning');
+            budgetLeft.parentElement.parentElement.classList.add('alert-danger'); 
+        // check when 50% left
+        } else if ((budget.budget / 2) > budgetLeftDollars) {
+            budgetLeft.parentElement.parentElement.classList.remove('alert-success');
+            budgetLeft.parentElement.parentElement.classList.add('alert-warning'); 
+        }
+        
+        
     }
 }
 
@@ -61,7 +91,9 @@ function eventListeners() {
         if (expenseName === '' || amount === '') {
             html.printMessage('all fields are mandatory', 'alert-danger');    
         } else {
-            
+            html.addExpenseToList(expenseName, amount);
+            html.trackBudget(amount);
+            html.printMessage('added...', 'alert-success');    
         }
     });
     
