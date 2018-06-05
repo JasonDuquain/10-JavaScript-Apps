7d3f1fc351acef6a9bfdc4b7680f260ea023aa6b
jasonduquain <jduquain@gmail.com>
2018-06-05 09:20:23 -0400

    finished project

--- Send an Email/js/app.js
+++ Send an Email/js/app.js
@@ -6,10 +6,8 @@ const sendBtn = document.getElementById('sendBtn'),
       resetBtn = document.getElementById('resetBtn'),
       sendEmailForm = document.getElementById('email-form');
 
-
 // Event Listeners
 eventListeners();
-
 function eventListeners() {
      // App Init
      document.addEventListener('DOMContentLoaded', appInit);
@@ -31,20 +29,32 @@ function eventListeners() {
 function appInit() {
      // disable the send button on load
      sendBtn.disabled = 'true';
+     
 }
 
 function sendEmail(e) {
-     
-
+    e.preventDefault();
+    
      // show the spinner
      const spinner = document.querySelector('#spinner');
-     
+     spinner.style.display = 'block';
 
      // Show the image
-     
+     const sendEmailImg = document.createElement('img');
+    sendEmailImg.setAttribute('src', 'img/mail.gif');
+    sendEmailImg.style.display = 'block';
 
      // Hide Spinner then show the send Email image
-     
+     setTimeout(() => {
+        spinner.style.display = 'none'; 
+         
+         document.querySelector('#loaders').append(sendEmailImg);
+         
+         setTimeout(() => {
+             sendEmailForm.reset();
+             sendEmailImg.style.display = 'none';
+         }, 2500);
+     }, 2000);
 }
 
 // Validate the fields
@@ -60,13 +70,16 @@ function validateField() {
      }
 
      // Both will return errors, then check if there're any errors
-     
+     errors = document.querySelectorAll('.error');
 
      // Check that the inputs are not empty
-     
+     if (email.value !== '' && subject.value !== '' && message.value !== '') {
+         if (errors.length === 0) {
+             sendBtn.disabled =  false;
+         }
+     }
 }
 
-// Validate the Length of the fields
 function validateLength(field) {
      if (field.value.length > 0) {
          field.style.borderBottomColor = 'green';
@@ -77,7 +90,6 @@ function validateLength(field) {
      }
 }
 
-// validate email (checks for @ in the value )
 function validateEmail(field) {
     let emailText = field.value;
     if (emailText.indexOf('@') !== -1) {
@@ -90,8 +102,7 @@ function validateEmail(field) {
     
 }
 
-// Reset the form
 function resetForm(e) {
-      
+     sendEmailForm.reset(); 
 
 }
